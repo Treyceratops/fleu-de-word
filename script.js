6d26d2408e1f80a241c8a0891060c036e8f8debf
treyceratops <treytowers@yahoo.com>
2022-06-08 13:41:55 -0700

    dry code, deploy

--- script.js
+++ script.js
@@ -19,30 +19,34 @@ const MAX_WRONG_GUESSES = 5
 
 /*----- app's state (variables) -----*/
 
-// pick a random word from array
+// holds variable to pick a random word from array
 let secretWord;
 
 // setup array to show # of empty spaces per word '_' (arr.length)
 let lengthOfWord;
 
-// variable that holds guess from keyboard
+// holds variable of current guessed letter
 let currentGuess;
 
-// variable that tracks # of wrong guesses
+// holds variable that tracks # of wrong guesses
 let currWrongGuesses;
 
-// variable holds true or false depending on whether current guess is in word
+// holds variable as true or false depending on whether current guess is in secretWord
 let includedLetter;
+
+// holds variables of correctly guessed letters
+let correctLetters = [];
+
+// holds variables of incorrectly guessed letters
+let incorrectLetter = [];
 
 
 // (icebox: hints)
 
 /*----- cached element references -----*/
 
-// gets keyboard rows from DOM
-const keyboard1 = document.getElementById('keyboard1');
-const keyboard2 = document.getElementById('keyboard2');
-const keyboard3 = document.getElementById('keyboard3');
+// gets display screen from DOM 
+const displayScreen = document.getElementById('displayScreen')
 
 // gets reset button from DOM
 const resetButton = document.getElementById('reset-button');
@@ -51,17 +55,14 @@ const resetButton = document.getElementById('reset-button');
 const roses = document.getElementById('roses');
 
 // current guessed letter
-const currentGuessEl = document.getElementById('current-guess');
+// const currentGuessEl = document.getElementById('current-guess');
 
-// display screen (icebox: update display to say `You have made ${# of wrong guesses}` or `you have `${x}` amount of guesses left`)
-const displayScreen = document.getElementById('displayScreen')
+// gets keyboard rows from DOM
+const keyboard1 = document.getElementById('keyboard1');
+const keyboard2 = document.getElementById('keyboard2');
+const keyboard3 = document.getElementById('keyboard3');
 
 /*----- event listeners -----*/
-
-// allows keyboard to be clicked, runs handleClick function
-keyboard1.addEventListener('click', handleClick);
-keyboard2.addEventListener('click', handleClick);
-keyboard3.addEventListener('click', handleClick);
 
 // allows reset button to be clicked, runs resetGame function
 resetButton.addEventListener('click', resetGame);
@@ -70,28 +71,44 @@ resetButton.addEventListener('click', resetGame);
 
 // render current guessed word progress or blank spots per length
 
+// allows keyboard to be clicked, runs handleClick function
+function addKeyboard() {
+    keyboard1.addEventListener('click', handleClick);
+    keyboard2.addEventListener('click', handleClick);
+    keyboard3.addEventListener('click', handleClick);
+}
+// removes event listener to keyboard
+function removeKeyboard() {
+    keyboard1.removeEventListener('click', handleClick);
+    keyboard2.removeEventListener('click', handleClick);
+    keyboard3.removeEventListener('click', handleClick);
+}
+
 // sets game up to starting state
 function initGame() {
+    roses.src = DECAYED_FLOWERS[0]
     secretWord = getSecretWord()
     currWrongGuesses = 0
-    renderDisplay()
     currentGuess = ''
-    roses.src = DECAYED_FLOWERS[0]
+    renderDisplay()
+    correctLetters = []
+    incorrectLetter = []
     generateKeyboard1()
     generateKeyboard2()
     generateKeyboard3()
+    addKeyboard()
 }
 
 // resets game
 function resetGame() {
+    roses.src = DECAYED_FLOWERS[0]
     secretWord = getSecretWord()
     currWrongGuesses = 0
-    renderDisplay()
     currentGuess = ''
-    roses.src = DECAYED_FLOWERS[0]
-    keyboard1.addEventListener('click', handleClick);
-    keyboard2.addEventListener('click', handleClick);
-    keyboard3.addEventListener('click', handleClick);
+    renderDisplay()
+    correctLetters = []
+    incorrectLetter = []
+    addKeyboard()
 }
 // randomizes secret word
 function getSecretWord() {
@@ -129,7 +146,7 @@ function generateKeyboard3() {
     })
 
 }
-// when keyboard is clicked, updates the inner text and runs updateCurrentGuess function
+// when keyboard is clicked, updates the inner text and runs updateCurrentGuess function with clicked letter
 function handleClick(evt) {
     updateCurrentGuess(evt.target.innerText)
 }
@@ -161,30 +178,15 @@ function dealResults() {
 
 // changes image based on incorrect guesses
 function phaseFlower() {
-    if (currWrongGuesses === 1) {
-        roses.src = DECAYED_FLOWERS[1]
-        renderDisplay()
-    } if (currWrongGuesses === 2) {
-        roses.src = DECAYED_FLOWERS[2]
-        renderDisplay()
-    } if (currWrongGuesses === 3) {
-        roses.src = DECAYED_FLOWERS[3]
-        renderDisplay()
-    } if (currWrongGuesses === 4) {
-        roses.src = DECAYED_FLOWERS[4]
-        renderDisplay()
-    } if (currWrongGuesses === 5) {
-        roses.src = DECAYED_FLOWERS[5]
-        loseGame()
-    }
+    roses.src = DECAYED_FLOWERS[currWrongGuesses]
+    renderDisplay()
+    loseGame()
 }
 
 // in event of a won game, deactivates keyboard & displays win
 // function winGame() {
 //     if (some winning condition) {
-//         keyboard1.removeEventListener('click', handleClick);
-//         keyboard2.removeEventListener('click', handleClick);
-//         keyboard3.removeEventListener('click', handleClick);
+//         removeKeyboard()
 //         displayScreen.innerText = 'Felicitations! We have a winner!'
 //     }
 // }
@@ -192,9 +194,7 @@ function phaseFlower() {
 // in event of a lost game, deactivates keyboard & displays loss
 function loseGame() {
     if (currWrongGuesses === 5) {
-        keyboard1.removeEventListener('click', handleClick);
-        keyboard2.removeEventListener('click', handleClick);
-        keyboard3.removeEventListener('click', handleClick);
+        removeKeyboard()
         displayScreen.innerText = 'Fin... Play again?'
     }
 }
