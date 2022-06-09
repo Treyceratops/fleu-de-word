f113ba43669dee204a319793b1e32a24a164879c
treyceratops <treytowers@yahoo.com>
2022-06-09 09:38:13 -0700

    pseudocode

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 /*----- constants -----*/
 
-// letters allowed to use
+// defines letters allowed to be used
 const ALLOWED_LETTERS = [['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'], ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
 ['z', 'x', 'c', 'v', 'b', 'n', 'm']];
 
-// create an array of words
+// defines an array of words
 const WORD_BANK = ['FLOWER', 'GARDEN', 'STAMEN', 'PETAL', 'LEAF', 'POLLEN', 'STEM', 'THORN', 'ROOT', 'BLOSSOM', 'ROSE', 'ORCHID', 'DAISY', 'SUNFLOWER', 'LILAC', 'LAVENDER', 'TULIP', 'LILY', 'PEONY', 'GARDENIA', 'CARNATION', 'DAHLIA', 'PANSY', 'AZALEA', 'GERANIUM', 'SNAPDRAGON', 'IRIS', 'POPPY', 'DAFFODIL', 'PETUNIA', 'VIOLET', 'HIBISCUS', 'PLUMERIA'];
 
-// holds flowers in diff states of decay's images
+// defines flowers in diff states of decay's images
 const DECAYED_FLOWERS = ['https://i.imgur.com/sSlpUiV.jpg?2', 'https://i.imgur.com/r8SL3dO.jpg?1', 'https://i.imgur.com/0v8IfiF.jpg?1', 'https://i.imgur.com/bft2YiS.jpg?2', 'https://i.imgur.com/mT7c7QG.jpg?2', 'https://i.imgur.com/hNeSeqR.jpg?2'];
 
 // defines the most incorrect guesses that can be made
@@ -18,7 +18,7 @@ const MAX_WRONG_GUESSES = 5
 // holds variable to pick a random word from array
 let secretWord;
 
-// setup array to show # of empty spaces per word '_' (arr.length)
+// holds array to show # of empty spaces per word
 let lengthOfWord;
 
 // holds variable of current guessed letter
@@ -36,8 +36,6 @@ let correctLetters = [];
 // holds variables of incorrectly guessed letters
 let incorrectLetters = [];
 
-// (icebox: hints)
-
 /*----- cached element references -----*/
 
 // gets display screen from DOM 
@@ -49,9 +47,6 @@ const resetButton = document.getElementById('reset-button');
 // gets image div from DOM
 const roses = document.getElementById('roses');
 
-// current guessed letter
-// const currentGuessEl = document.getElementById('current-guess');
-
 // gets keyboard rows from DOM
 const keyboard1 = document.getElementById('keyboard1');
 const keyboard2 = document.getElementById('keyboard2');
@@ -62,14 +57,12 @@ const guessDiv = document.getElementById('current-guess')
 
 /*----- event listeners -----*/
 
-// allows reset button to be clicked, runs resetGame function
+// listens to reset button when clicked, runs resetGame function
 resetButton.addEventListener('click', resetGame);
 
 /*----- functions -----*/
 
-// render current guessed word progress or blank spots per length
-
-// allows keyboard to be clicked, runs handleClick function
+// listens to keyboard being clicked, runs handleClick function
 function addKeyboard() {
     keyboard1.addEventListener('click', handleClick);
     keyboard2.addEventListener('click', handleClick);
@@ -81,7 +74,6 @@ function removeKeyboard() {
     keyboard2.removeEventListener('click', handleClick);
     keyboard3.removeEventListener('click', handleClick);
 }
-
 // sets game up to starting state
 function initGame() {
     roses.src = DECAYED_FLOWERS[0]
@@ -97,8 +89,7 @@ function initGame() {
     addKeyboard()
     generateSecretSpots()
 }
-
-// resets game
+// resets game to starting state
 function resetGame() {
     roses.src = DECAYED_FLOWERS[0]
     secretWord = getSecretWord()
@@ -118,43 +109,38 @@ function getSecretWord() {
     const randomInt = Math.floor(Math.random() * WORD_BANK.length)
     return WORD_BANK[randomInt]
 }
-
+// generates top row of keyboard
 function generateKeyboard1() {
-    // generates top row of keyboard
     ALLOWED_LETTERS[0].forEach(function (letter) {
         const cell1 = document.createElement('div')
         cell1.innerText = letter.toUpperCase()
         cell1.classList.add('cell')
         keyboard1.appendChild(cell1)
     })
-
 }
+// generates middle row of keyboard
 function generateKeyboard2() {
-    // generates middle row of keyboard
     ALLOWED_LETTERS[1].forEach(function (letter) {
         const cell2 = document.createElement('div')
         cell2.innerText = letter.toUpperCase()
         cell2.classList.add('cell')
         keyboard2.appendChild(cell2)
     })
-
 }
+// generates bottom row of keyboard
 function generateKeyboard3() {
-    // generates bottom row of keyboard
     ALLOWED_LETTERS[2].forEach(function (letter) {
         const cell3 = document.createElement('div')
         cell3.innerText = letter.toUpperCase()
         cell3.classList.add('cell')
         keyboard3.appendChild(cell3)
     })
-
 }
 // when keyboard is clicked, updates the inner text and runs updateCurrentGuess function with clicked letter
 function handleClick(evt) {
     updateCurrentGuess(evt.target)
 }
-
-// updates current guess to chosen letter
+// stops keys from being clicked again, checks conditions of other functions
 function updateCurrentGuess(cell) {
     currentGuess = cell.innerText
     if (correctLetters.some((letter) => letter === currentGuess) || incorrectLetters.some((letter) => letter === currentGuess)) {
@@ -168,22 +154,20 @@ function updateCurrentGuess(cell) {
     phaseFlower()
     winGame()
 }
+// generates cells for the secret word
 function generateSecretSpots() {
-    // generates top row of keyboard
     guessDiv.innerHTML = ''
     secretWord.split('').forEach(function (letter) {
         const cell = document.createElement('div')
         cell.classList.add('cell')
         guessDiv.appendChild(cell)
     })
-
 }
-// searches to see if currentGuess is in word
+// searches to see if current guess is in secret word
 function checkForLetter() {
     includedLetter = secretWord.includes(currentGuess)
 }
-
-// updates correct or incorrect guesses
+// sorts correct or incorrect guesses, changes key color, adds correct results to secret word
 function dealResults(cell) {
     if (includedLetter === true) {
         cell.style.color = 'green'
@@ -195,6 +179,7 @@ function dealResults(cell) {
         incorrectLetters.push(currentGuess)
     }
 }
+// adds letter to word when correctly guessed
 function addResults(guessedLetter) {
     secretWord.split('').forEach((currLetter, idx) => {
         if (currLetter === guessedLetter) {
@@ -202,14 +187,12 @@ function addResults(guessedLetter) {
         }
     })
 }
-
 // changes image based on incorrect guesses
 function phaseFlower() {
     roses.src = DECAYED_FLOWERS[currWrongGuesses]
     renderDisplay()
     loseGame()
 }
-
 // in event of a won game, deactivates keyboard & displays win
 function winGame() {
     let filledCells = true
@@ -223,7 +206,6 @@ function winGame() {
         displayScreen.innerText = 'Felicitations! We have a winner!'
     }
 }
-
 // in event of a lost game, deactivates keyboard & displays loss
 function loseGame() {
     if (currWrongGuesses === 5) {
@@ -235,5 +217,4 @@ function loseGame() {
 function renderDisplay() {
     displayScreen.innerText = `Make a guess! Your word is ${secretWord.length} letters long, you have ${5 - currWrongGuesses} guesses left.`
 }
-
 initGame() 
